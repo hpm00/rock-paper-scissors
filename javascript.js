b2a17a78c7c120044969afe2281d19d00958ef66
husna <hpm25@georgetown.edu>
2022-07-29 19:35:15 -0700

    Create game

--- javascript.js
+++ javascript.js
@@ -1,41 +1,50 @@
 const selection = ['rock', 'paper', 'scissors'];
 
-function userChoice() {
-    let answer = prompt("Choose Rock, Paper, or Scissors: ", '');
-    return answer.toLowerCase();
-}
-
 function getComputerChoice() {
     return selection[Math.floor(Math.random() * selection.length)];
 }
 
-const computerSelection = getComputerChoice();
-const playerSelection = userChoice()
+getComputerChoice();
+
+let computerSelection = getComputerChoice();
+let playerSelection = prompt("Choose Rock, Paper, or Scissors: ", '').toLowerCase();
+
+let playerScore = 0;
+let computerScore = 0;
+let draws = 0;
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
-        alert("It's a tie. You both chose " + playerSelection)
+        console.log(++draws)
+        console.log("It's a tie. You both chose " + playerSelection + "." + " Score: " +
+        "Tie: " + draws + " Computer: " + computerScore + " You: " + playerScore)
     }
     else if (playerSelection == 'rock' && computerSelection == 'scissors' || 
     playerSelection == 'scissors' && computerSelection == 'paper' ||
     playerSelection == 'paper' && computerSelection == 'rock') {
-        alert("You win! " + playerSelection + " beats " + computerSelection)
-    }
-    else if (playerSelection == ''|| playerSelection !== 'rock', 'paper', 'scissors') {
-        alert("Invalid")
+        console.log(++playerScore)
+        console.log("You win! " + playerSelection + " beats " + computerSelection + 
+        "." + " Score: " + "Tie: " + draws + " Computer: " + computerScore +
+         " You: " + playerScore)
     }
     else {
-        alert("You lose! " + computerSelection + " beats " + playerSelection)
+        console.log(++computerScore)
+        console.log("You lose! " + computerSelection + " beats " + playerSelection +
+        " ." + " Score: " + "Tie: " + draws + " Computer: " + computerScore +
+         " You: " + playerScore)
     }
 }
+
+playRound();
 
 function game() {
     for (i = 0; i<5; i++) {
-        function playRound()
+        const playerSelection = prompt("Choose Rock, Paper, or Scissors: ", '').toLowerCase();
+        console.log(getComputerChoice())
+        console.log(playRound(playerSelection, computerSelection))
+        console.log("your score = " + playerScore)
+        console.log("Computer's score = " + computerScore)
     }
 }
 
-console.log(playRound(playerSelection,computerSelection))
-
-// play 5 rounds // MEDIUM
-// display winner at the end of the 5 rounds // point system HARDEST
+game();
